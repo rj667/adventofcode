20580a24949c3df506f0ca0b4ede86a9785dba31
Robert Scheer <robertscheer@freedom.nl>
2023-12-08 23:26:48 +0100

    day 5. part 2 did not complete

---
+++ day05-2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pprint import pprint
+
+# This brute-force solution takes too long...
+
+try:
+    infile = sys.argv[1]
+except IndexError:
+    infile = sys.argv[0].split('-')[0] + '.input.txt'
+
+t0 = time.perf_counter()
+
+
+def get_map_value(number, ranges):
+    for dest, source, length in ranges:
+        if source <= number < source + length:
+            return dest + number - source
+    return number
+    
+
+maps = {}
+mapnames = []
+with open(infile, 'r') as input:
+    seeds = list(map(int, input.readline().rstrip().split(':')[1].strip().split()))
+    for line in input:
+        if not line.rstrip():
+            mapname = input.readline().rstrip().split()[0].split('-')[-1]
+            mapnames.append(mapname)
+            continue
+        if mapname not in maps:
+            maps[mapname] = []
+        maps[mapname].append(list(map(int, line.rstrip().split())))
+
+numbers = []
+for start, length in zip(seeds[::2], seeds[1::2]):
+    for number in range(start, start + length):
+        for mapname in mapnames:
+            number = get_map_value(number, maps[mapname])
+        numbers.append(number)
+
+answer = min(numbers)
+
+t1 = time.perf_counter()
+print(f"Answer: {answer}")
+print(f"Execution time: {t1 - t0:.6f} seconds")
