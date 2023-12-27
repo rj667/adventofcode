d44cc749e6d94eea919cac30ee666541029ea99e
Robert Scheer <robertscheer@xs4all.net>
2023-12-27 21:55:13 +0100

    new approach day05-2. Example input works, personal input doesn't

--- day05-2.py
+++ day05-2.py
@@ -26,6 +26,7 @@ mapnames = []
 with open(infile, 'r') as input:
     seeds = list(map(int, input.readline().rstrip().split(':')[1].strip().split()))
     for line in input:
+        if line[0] == '#': continue
         # read map name
         if not line.rstrip():
             name = input.readline().rstrip().split()[0].split('-')[-1]
@@ -45,55 +46,66 @@ for name in mapnames:
 print("mapranges:\n", pformat(mapranges, sort_dicts=False))
 
 def src_to_dest (input_range, compare_ranges):
+    print('input_range: ', input_range)
+    print('compare_ranges: ', pformat(compare_ranges))
     output_ranges = []
     input_start, input_length = input_range
-    for range_dest, range_start, range_length in compare_ranges:
-        print('input range:', (input_start, input_length))
-        print('compare range:', (range_start, range_length))
-        if input_start >= range_start and input_start < range_start + range_length:
+    for compare_range in compare_ranges:
+        input_range = [input_start, input_length]
+        input_end = input_start + input_length - 1
+        print(f'input range: {input_range} = {input_start}-{input_end}')
+        compare_dest, compare_start, compare_length = compare_range
+        compare_end = compare_start + compare_length - 1
+        print(f'compare range: {compare_range} = {compare_start}-{compare_end}')
+        if input_start >= compare_start and input_start < compare_start + compare_length:
             # input start is in range
             print('input start IS in range')
-            if input_start + input_length <= range_start + range_length:
+            if input_start + input_length <= compare_start + compare_length:
                 # input end is in range
                 print('input end IS in range')
-                new_range = (input_start + (range_dest - range_start), input_length)
-                print('new range:', new_range)
-                #output_ranges.append((input_start + (range_dest - range_start), input_length))
+                new_range = [input_start + (compare_dest - compare_start), input_length]
+                print('transformed whole range:', new_range)
                 output_ranges.append(new_range)
+                input_start = input_start + input_length
+                input_length = input_length - compare_length
                 break
             else:
                 print('input end IS NOT in range')
-                new_range = (input_start + (range_dest - range_start), range_length)
-                print('new range:', new_range)
-                #output_ranges.append((input_start + (range_dest - range_start), range_length))
+                new_range = [input_start + (compare_dest - compare_start), compare_length]
+                print('transformed partial range: ', new_range)
                 output_ranges.append(new_range)
-                input_start += range_length
-                input_length -= range_length
-                print('new range:', (input_start, input_length))
+                rest_range = (input_start + (compare_dest - compare_start - 1), input_length - compare_length)
+                print('rest range:', rest_range)
+                input_start, input_length = rest_range
         else:
             # skip range
             print('input start IS NOT in range: SKIPPING')
             continue
     else:
-        print('rest range:', (input_start, input_length))
-        output_ranges.append((input_start, input_length))
+        if input_length > 0:
+            print('else range:', [input_start, input_length])
+            output_ranges.append([input_start, input_length])
     print('output_ranges:', output_ranges)
     return output_ranges
 
 
-ranges_todo = list(zip(seeds[::2], seeds[1::2]))
-for map_name in mapnames:
-    print('MAP:', map_name, 'ranges_todo', pformat(ranges_todo))
-    ranges_done = []
-    for range_todo in ranges_todo:
-        ranges_done += src_to_dest(range_todo, mapranges[map_name])
-    print('MAP:', map_name, 'ranges_done', pformat(ranges_done))
-    ranges_todo = ranges_done
+seeds = list(zip(seeds[::2], seeds[1::2]))
+result_ranges = []
+for seed_start, seed_length in seeds:
+    ranges_todo = [[seed_start, seed_length]]
+    for map_name in mapnames:
+        print('BEGIN MAP:', map_name, 'ranges_todo', ranges_todo)
+        ranges_done = []
+        for range_todo in ranges_todo:
+            ranges_done += src_to_dest(range_todo, mapranges[map_name])
+        print('END MAP:', map_name, 'ranges_done', pformat(ranges_done))
+        ranges_todo = ranges_done
+    result_ranges += ranges_done
 
-print('ranges_todo:', ranges_todo)
+print('result_ranges:', pformat(result_ranges))
+answer = min([result_range[0] for result_range in result_ranges])
 
 
-answer = 0
 
 t1 = time.perf_counter()
 print(f"Answer: {answer}")
