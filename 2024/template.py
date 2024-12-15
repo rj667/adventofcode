4aedda059f643300a0680d60941cc8dd3be93e64
Robert Scheer <robertscheer@freedom.nl>
2024-12-15 12:08:38 +0100

    simplified template

--- 2024/template.py
+++ 2024/template.py
@@ -4,20 +4,17 @@ import sys
 import time
 from pprint import pprint
 
-try:
-    infile = sys.argv[1]
-except IndexError:
-    infile = sys.argv[0].split('-')[0] + '.input.txt'
-
 t0 = time.perf_counter()
+
+infile = sys.argv[1:] and sys.argv[1] or "input.txt"
 
 total = 0
 with open(infile, 'r') as file:
-    my_input = file.read()
-    #my_input = [line.strip() for line in file]
-    #my_input = [list(map(str, line.split())) for line in file]
+    #data = file.read()
+    data = [line.strip() for line in file]
+    #data = [list(map(str, line.split())) for line in file]
 
-print(my_input)
+pprint(data)
 t1 = time.perf_counter()
 print(f"Answer: {total}")
 print(f"Execution time: {t1 - t0:.6f} seconds")
