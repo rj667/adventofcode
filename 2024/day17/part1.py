0abc0651a31615ba35d0aedebf67c21c711e74d1
Robert Scheer <robertscheer@freedom.nl>
2024-12-20 22:13:40 +0100

    rewritten output of day17/part1

--- 2024/day17/part1.py
+++ 2024/day17/part1.py
@@ -18,8 +18,8 @@ with open(infile, 'r') as file:
             register[match.group(1)] = int(match.group(2))
         if (match := re.match(r'Program: (.*)', line)):
             program = [int(char) for char in match.group(1).replace(',','')]
-print(register)
-print(program)
+print(f'program: {program}')
+print(f'register:   {register}')
 
 def get_literal(operand):
     return operand
@@ -39,80 +39,49 @@ def get_combo(operand, register):
     return operand
 
 def instruction_adv(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['A'] = register['A'] // 2 ** get_combo(operand, register)
-    print('adv', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_bxl(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['B'] = register['B'] ^ get_literal(operand)
-    print('bxl', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_bst(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['B'] = get_combo(operand, register) % 8
-    print('bst', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_jnz(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
     if register['A'] != 0:
         pointer = get_literal(operand)
     else:
         pointer += 2
-    print('jnz', operand, old_pointer, pointer, old_register, register)
     return pointer, register
 
 def instruction_bxc(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['B'] = register['B'] ^ register['C']
-    print('bxc', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_out(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     output.append(get_combo(operand, register) % 8)
-    print('out', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_bdv(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['B'] = register['A'] // 2 ** get_combo(operand, register)
-    print('bdv', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 def instruction_cdv(operand, pointer, register):
-    old_pointer  = pointer
-    old_register = register.copy()
-    pointer += 2
     register['C'] = register['A'] // 2 ** get_combo(operand, register)
-    print('cdv', operand, old_pointer, pointer, old_register, register)
-    return pointer, register
+    return pointer+2, register
 
 instruction = [
-    instruction_adv,
-    instruction_bxl,
-    instruction_bst,
-    instruction_jnz,
-    instruction_bxc,
-    instruction_out,
-    instruction_bdv,
-    instruction_cdv,
+    instruction_adv,    #0
+    instruction_bxl,    #1
+    instruction_bst,    #2
+    instruction_jnz,    #3
+    instruction_bxc,    #4
+    instruction_out,    #5
+    instruction_bdv,    #6
+    instruction_cdv,    #7
 ]
 
 pointer = 0
@@ -120,10 +89,11 @@ output = []
 while pointer < len(program):
     opcode = program[pointer]
     operand = program[pointer+1]
-    print(opcode, operand)
+    print(f'{opcode} {instruction[opcode].__name__[-3:]} {operand} {pointer:3} {str(register):45}', end=' ')
     pointer, register = instruction[opcode](operand, pointer, register)
+    print(f'{pointer:2} {str(register):45s} {output}')
 
+output = ','.join(str(_) for _ in output)
 t1 = time.perf_counter()
-print(f"Answer: {output}")
-print(f"Answer: {','.join(str(_) for _ in output)}")
+print(f"Output: {output}")
 print(f"Execution time: {t1 - t0:.6f} seconds")
