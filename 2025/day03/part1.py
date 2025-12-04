5af52f1ce220b9029f38e1828b5bcc414fd3f51c
Robert Scheer <robertscheer@freedom.nl>
2025-12-04 13:15:43 +0100

    day03 done

---
+++ 2025/day03/part1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pprint import pprint
+
+t0 = time.perf_counter()
+
+infile = sys.argv[1:] and sys.argv[1] or "input.txt"
+
+total = 0
+with open(infile, 'r') as file:
+    #data = file.read()
+    data = [line.strip() for line in file]
+    #data = [list(map(str, line.split())) for line in file]
+
+for line in data:
+    bank = list(map(int, list(line)))
+    bat1 = max(bank[:len(bank)-1])
+    pos1 = bank.index(bat1)
+    bat2 = max(bank[pos1+1:])
+    joltage = int(str(bat1) + str(bat2))
+    total += joltage
+
+t1 = time.perf_counter()
+print(f"Answer: {total}")
+print(f"Execution time: {t1 - t0:.6f} seconds")
---
+++ 2025/day03/part2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pprint import pprint
+
+t0 = time.perf_counter()
+
+infile = sys.argv[1:] and sys.argv[1] or "input.txt"
+
+total = 0
+with open(infile, 'r') as file:
+    #data = file.read()
+    data = [line.strip() for line in file]
+    #data = [list(map(str, line.split())) for line in file]
+
+needed = 12
+for line in data:
+    bank = list(map(int, list(line)))
+    #print(f'BANK: {bank}')
+    joltage = ""
+    pos = -1
+    for n in range(needed):
+        select_from = bank[pos+1:len(bank)-needed+n+1]
+        battery = max(bank[pos+1:len(bank)-needed+n+1])
+        joltage += str(battery)
+        #print(f"      {select_from}, battery: {battery}, joltage: {joltage}")
+        pos = bank.index(battery, pos+1)
+    total += int(joltage)
+
+t1 = time.perf_counter()
+print(f"Answer: {total}")
+print(f"Execution time: {t1 - t0:.6f} seconds")
