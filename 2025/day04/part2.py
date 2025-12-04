2cae6e6eaf87b81e8647e3bf8d0507280c10d4cd
Robert Scheer <robertscheer@freedom.nl>
2025-12-04 22:29:02 +0100

    day04 done

---
+++ 2025/day04/part2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pprint import pprint
+
+t0 = time.perf_counter()
+
+infile = sys.argv[1:] and sys.argv[1] or "input.txt"
+
+total = 0
+with open(infile, 'r') as file:
+    grid = [list(line.strip()) for line in file]
+
+def print_grid():
+    for y in range(len(grid)):
+        print(''.join(grid[y]))
+    print()
+
+roll = "@"
+removed = "x"
+empty = "."
+
+#print_grid()
+while True:
+    removed_rolls_this_iteration = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] != roll:
+                continue
+            surrounding_rolls = 0
+            if y > 0 and x > 0:
+                if grid[y-1][x-1] != empty:
+                    surrounding_rolls += 1
+            if y > 0:
+                if grid[y-1][x] != empty:
+                    surrounding_rolls += 1
+            if y > 0 and x < len(grid[y]) - 1:
+                if grid[y-1][x+1] != empty:
+                    surrounding_rolls += 1
+            if x > 0:
+                if grid[y][x-1] != empty:
+                    surrounding_rolls += 1
+            if x < len(grid[y]) - 1:
+                if grid[y][x+1] != empty:
+                    surrounding_rolls += 1
+            if y < len(grid) - 1 and x > 0:
+                if grid[y+1][x-1] != empty:
+                    surrounding_rolls += 1
+            if y < len(grid) - 1:
+                if grid[y+1][x] != empty:
+                    surrounding_rolls += 1
+            if y < len(grid) - 1 and x < len(grid[y]) - 1:
+                if grid[y+1][x+1] != empty:
+                    surrounding_rolls += 1
+            if surrounding_rolls < 4:
+                grid[y][x] = removed
+                removed_rolls_this_iteration += 1
+    #print_grid()
+    if removed_rolls_this_iteration == 0:
+        break
+    total += removed_rolls_this_iteration
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == removed:
+                grid[y][x] = empty
+
+
+t1 = time.perf_counter()
+print(f"Answer: {total}")
+print(f"Execution time: {t1 - t0:.6f} seconds")
