b7f343211fdc81828d32cddb458658f2ee6678bc
Robert Scheer <robertscheer@freedom.nl>
2025-12-12 09:03:40 +0100

    day09 part1 in progress

--- 2025/day09/part1.py
+++ 2025/day09/part1.py
@@ -13,6 +13,15 @@ with open(infile, 'r') as file:
     data = [tuple(map(int, line.strip().split(","))) for line in file]
 
 print(data)
+seen_tiles = []
+largest_area = 0
+for tile in data:
+    for seen in seen_tiles:
+        area = (abs(tile[0]-seen[0])+1)*(abs(tile[1]-seen[1])+1)
+        if largest_area < area:
+            largest_area = area
+    seen_tiles.append(tile)
+total = largest_area
 
 t1 = time.perf_counter()
 print(f"Answer: {total}")
